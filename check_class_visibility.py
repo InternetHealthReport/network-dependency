0dcb35be765a538f6db7ab7dbdca89839a91b0bb
Malte Appel <malte@iij-ii.co.jp>
2021-07-28 15:04:44 +0900

    Make script to check visibility class independent.

--- check_class_visibility.py
+++ check_class_visibility.py
@@ -13,12 +13,18 @@ from network_dependency.utils.helper_functions \
 DATE_FMT = '%Y-%m-%dT%H:%M'
 
 
+def csv_converter(entry: str) -> list:
+    return [e.strip() for e in entry.split(',')]
+
+
 def check_config(config_path: str) -> configparser.ConfigParser:
-    config = configparser.ConfigParser()
+    config = configparser.ConfigParser(converters={'csv': csv_converter})
     config.read(config_path)
     try:
         config.get('input', 'visibility_topic')
         config.get('input', 'classification_topic')
+        config.getcsv('input', 'included_classes')
+        config.getcsv('input', 'excluded_classes')
         config.get('output', 'kafka_topic')
         config.getint('settings', 'lookback_days')
         config.get('kafka', 'bootstrap_servers')
@@ -34,26 +40,26 @@ def check_config(config_path: str) -> configparser.ConfigParser:
     return config
 
 
-def read_bgp_only_dependencies(reader: KafkaReader) -> set:
-    bgp_only = set()
-    others = set()
+def read_class_dependencies(reader: KafkaReader,
+                            included_classes: list,
+                            excluded_classes: list) -> set:
+    included = set()
+    excluded = set()
     for msg in reader.read():
-        if check_keys(['equal', 'mismatched', 'tr_only', 'bgp_only'], msg):
+        if check_keys(included_classes + excluded_classes, msg):
             logging.warning(f'Skipping message with missing fields: {msg}')
             continue
-        for asn, bgp_score, tr_score, rank in msg['equal']:
-            if asn not in others:
-                others.add(asn)
-        for asn, bgp_score, bgp_rank, tr_score, tr_rank in msg['mismatched']:
-            if asn not in others:
-                others.add(asn)
-        for asn, score, rank in msg['tr_only']:
-            if asn not in others:
-                others.add(asn)
-        for asn, score, rank in msg['bgp_only']:
-            if asn not in bgp_only:
-                bgp_only.add(asn)
-    return bgp_only - others
+        for class_name in included_classes:
+            for entry in msg[class_name]:
+                asn = entry[0]
+                if asn not in included:
+                    included.add(asn)
+        for class_name in excluded_classes:
+            for entry in msg[class_name]:
+                asn = entry[0]
+                if asn not in excluded:
+                    excluded.add(asn)
+    return included - excluded
 
 
 def read_visible_asns(reader: KafkaReader) -> dict:
@@ -71,7 +77,7 @@ def main() -> None:
     logging.basicConfig(
         format=log_fmt,
         level=logging.INFO,
-        filename='check_bgp_only_visibility.log',
+        filename='check_class_visibility.log',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
 
@@ -104,6 +110,8 @@ def main() -> None:
 
     visibility_topic = config.get('input', 'visibility_topic')
     classification_topic = config.get('input', 'classification_topic')
+    included_classes = config.getcsv('input', 'included_classes')
+    excluded_classes = config.getcsv('input', 'excluded_classes')
     bootstrap_servers = config.get('kafka', 'bootstrap_servers')
 
     visibility_reader = KafkaReader([visibility_topic], bootstrap_servers,
@@ -111,36 +119,37 @@ def main() -> None:
     with visibility_reader:
         visible_asns = read_visible_asns(visibility_reader)
 
-    bgp_only_reader = KafkaReader([classification_topic], bootstrap_servers,
-                                  lookup_ts * 1000, lookback_end_ts)
-    with bgp_only_reader:
-        bgp_only_asns = read_bgp_only_dependencies(bgp_only_reader)
+    class_reader = KafkaReader([classification_topic], bootstrap_servers,
+                               lookup_ts * 1000, lookback_end_ts)
+    with class_reader:
+        included_asns = read_class_dependencies(class_reader, included_classes,
+                                                excluded_classes)
 
-    bgp_only_writer = KafkaWriter(config.get('output', 'kafka_topic'),
-                                  bootstrap_servers,
-                                  num_partitions=10,
-                                  replication_factor=2,
-                                  # 2 months
-                                  config={'retention.ms': 5184000000})
+    class_writer = KafkaWriter(config.get('output', 'kafka_topic'),
+                               bootstrap_servers,
+                               num_partitions=10,
+                               replication_factor=2,
+                               # 2 months
+                               config={'retention.ms': 5184000000})
 
     threshold_steps = list(set(map(len, visible_asns.values())))
     threshold_steps.sort(reverse=True)
 
     msg = {'timestamp': lookup_ts}
 
-    logging.info(f'BGP only ASes: {len(bgp_only_asns):5d}')
+    logging.info(f'Included ASes: {len(included_asns):5d}')
     logging.info(f' Visible ASes: {len(visible_asns):5d}')
     logging.info(f'   Thresholds: {len(threshold_steps):5d}')
     remaining_visible_asns = visible_asns.keys()
-    remaining_bgp_only_asns = bgp_only_asns.copy()
-    with bgp_only_writer:
+    remaining_included_asns = included_asns.copy()
+    with class_writer:
         for threshold in threshold_steps:
             thresh_asns = {asn for asn in remaining_visible_asns
                            if len(visible_asns[asn]) == threshold}
             remaining_visible_asns -= thresh_asns
             visible_with_threshold = \
-                remaining_bgp_only_asns.intersection(thresh_asns)
-            remaining_bgp_only_asns -= thresh_asns
+                remaining_included_asns.intersection(thresh_asns)
+            remaining_included_asns -= thresh_asns
             logging.debug(f' threshold {threshold:2d}')
             logging.debug(f'  remaining visible: '
                           f'{len(remaining_visible_asns):5d}')
@@ -148,15 +157,15 @@ def main() -> None:
             logging.debug(f'  bgp visible w/ th: '
                           f'{len(visible_with_threshold):5d}')
             logging.debug(f'remaining invisible: '
-                          f'{len(remaining_bgp_only_asns):5d}')
+                          f'{len(remaining_included_asns):5d}')
             for asn in visible_with_threshold:
                 msg['asn'] = asn
                 msg['unique_ips'] = threshold
-                bgp_only_writer.write(asn, msg, lookup_ts * 1000)
-        for asn in remaining_bgp_only_asns:
+                class_writer.write(asn, msg, lookup_ts * 1000)
+        for asn in remaining_included_asns:
             msg['asn'] = asn
             msg['unique_ips'] = 0
-            bgp_only_writer.write(asn, msg, lookup_ts * 1000)
+            class_writer.write(asn, msg, lookup_ts * 1000)
 
 
 if __name__ == '__main__':
