8a9434e2995aa92c6eaecb705fba931549292f20
Malte Appel <malte@iij-ii.co.jp>
2022-03-31 06:25:51 +0000

    Fix log file name typo

--- filter_rib_topic.py
+++ filter_rib_topic.py
@@ -95,7 +95,7 @@ def main() -> None:
     logging.basicConfig(
         format=log_fmt,
         level=logging.INFO,
-        filename='filter_rip_topic.log',
+        filename='filter_rib_topic.log',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
     parser = argparse.ArgumentParser()
