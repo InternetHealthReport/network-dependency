4aa28a2d3ca2644de477fc11afed4fa099585e8e
Malte Tashiro <malte@iij.ad.jp>
2024-01-18 05:41:45 +0000

    Update filter_rib_topic.py
    - Add possibility to filter for probes
    - Add arguments for ASN and probe filter that overwrite configuration
    - Fix key encoding if prb_id is an int
    - Code formatting

--- filter_rib_topic.py
+++ filter_rib_topic.py
@@ -1,6 +1,7 @@
 import argparse
 import configparser
 import logging
+import os
 import sys
 from datetime import datetime, timezone
 
@@ -8,8 +9,7 @@ from confluent_kafka import OFFSET_BEGINNING, OFFSET_END
 
 from kafka_wrapper.kafka_reader import KafkaReader
 from kafka_wrapper.kafka_writer import KafkaWriter
-from network_dependency.utils.helper_functions import check_key, check_keys, \
-    parse_timestamp_argument
+from network_dependency.utils.helper_functions import check_key, check_keys, parse_timestamp_argument
 
 DATE_FMT = '%Y-%m-%dT%H:%M'
 
@@ -50,32 +50,40 @@ def check_config(config_path: str) -> configparser.ConfigParser:
     return config
 
 
-def read_asn_list(asn_list: str) -> set:
-    logging.info(f'Reading AS filter from: {asn_list}')
-    with open(asn_list, 'r') as f:
+def handle_filter_arg(arg: str, filter_type: str = str()) -> set:
+    if os.path.isfile(arg):
+        return read_filter_list(arg, filter_type)
+    if not arg.isdigit():
+        logging.error(f'{filter_type} filter is neither a file nor a number.')
+        return set()
+    return {arg}
+
+
+def read_filter_list(filter_list: str, filter_type: str = str()) -> set:
+    logging.info(f'Reading {filter_type} filter from: {filter_list}')
+    with open(filter_list, 'r') as f:
         ret = {line.strip() for line in f}
-    logging.info(f'Read {len(ret)} ASes')
+    logging.info(f'Read {len(ret)} entries')
     return ret
 
 
 def filter_msg(msg: dict,
                path_attributes: dict = None,
                excluded_path_attributes: set = None,
-               asn_list: set = None) -> (dict, int, int):
-    if check_keys(['rec', 'elements'], msg) \
-            or check_key('time', msg['rec']):
-        logging.error(f'Missing "rec", "time", or "elements" field in message: '
-                      f'{msg}')
+               asn_list: set = None,
+               prb_list: set = None) -> (dict, int, int):
+    if (check_keys(['rec', 'elements'], msg)
+            or check_key('time', msg['rec'])):
+        logging.error(f'Missing "rec", "time", or "elements" field in message: {msg}')
         return dict(), -1, None
     ret = msg.copy()
     timestamp = msg['rec']['time']
     prb_id = None
     ret['elements'] = list()
     for element in msg['elements']:
-        if check_keys(['peer_asn', 'fields'], element) \
-                or check_key('path-attributes', element['fields']):
-            logging.error(f'Missing "peer_asn", "fields" or "path-attributes" '
-                          f'field in message: {msg}')
+        if (check_keys(['peer_asn', 'fields'], element)
+                or check_key('path-attributes', element['fields'])):
+            logging.error(f'Missing "peer_asn", "fields" or "path-attributes" field in message: {msg}')
             continue
 
         peer_asn = element['peer_asn']
@@ -83,16 +91,16 @@ def filter_msg(msg: dict,
             logging.warning(f'Missing "prb_id" field in message: {msg}')
         else:
             if prb_id is not None:
-                logging.warning(f'Multiple elements with different probe ids in '
-                                f'message: {msg}')
+                logging.warning(f'Multiple elements with different probe ids in message: {msg}')
             prb_id = element['prb_id']
         if asn_list and peer_asn not in asn_list:
             continue
+        if prb_list and prb_id not in prb_list:
+            continue
 
         msg_path_attributes = element['fields']['path-attributes']
-        if excluded_path_attributes and \
-                excluded_path_attributes.intersection(
-                    msg_path_attributes.keys()):
+        if (excluded_path_attributes
+                and excluded_path_attributes.intersection(msg_path_attributes.keys())):
             continue
         if path_attributes:
             filtered = False
@@ -121,20 +129,19 @@ def main() -> None:
     )
     parser = argparse.ArgumentParser()
     parser.add_argument('config')
-    read_group_desc = """By using the --start, --end, and --timestamp options,
-                      only a specific range or an exact timestamp of TOPIC can
-                      be dumped. Either or both range options can be specified,
-                      but are exclusive with --timestamp. Timestamps can be
-                      specified as UNIX epoch in (milli)seconds or in the format
+    parser.add_argument('--asn-filter',
+                        help='single ASN or file containing a list of ASNs (takes precedence over config)')
+    parser.add_argument('--probe-filter',
+                        help='single probe ID or file containing a list of probe IDs (takes precedence over config)')
+    read_group_desc = """By using the --start, --end, and --timestamp options, only a
+                      specific range or an exact timestamp of TOPIC can be dumped.
+                      Either or both range options can be specified, but are exclusive
+                      with --timestamp. Timestamps can be specified as UNIX epoch in
+                      (milli)seconds or in the format
                       '%Y-%m-%dT%H:%M'."""
-    read_group = parser.add_argument_group('Interval specification',
-                                           description=read_group_desc)
-    read_group.add_argument('-st', '--start',
-                            help='start timestamp (default: read topic from '
-                                 'beginning)')
-    read_group.add_argument('-e', '--end',
-                            help='end timestamp (default: read topic to the '
-                                 'end)')
+    read_group = parser.add_argument_group('Interval specification', description=read_group_desc)
+    read_group.add_argument('-st', '--start', help='start timestamp (default: read topic from beginning)')
+    read_group.add_argument('-e', '--end', help='end timestamp (default: read topic to the end)')
     read_group.add_argument('-ts', '--timestamp', help='exact timestamp')
     args = parser.parse_args()
 
@@ -145,7 +152,7 @@ def main() -> None:
         sys.exit(1)
 
     if (args.start or args.end) and args.timestamp:
-        logging.error(f'Range and exact timestamp arguments are exclusive.')
+        logging.error('Range and exact timestamp arguments are exclusive.')
         sys.exit(1)
 
     start_ts = OFFSET_BEGINNING
@@ -170,30 +177,36 @@ def main() -> None:
         start_ts_dt = datetime.fromtimestamp(start_ts / 1000, tz=timezone.utc)
         logging.info(f'Start reading at {start_ts_dt.strftime(DATE_FMT)}')
     else:
-        logging.info(f'Start reading at beginning of topic')
+        logging.info('Start reading at beginning of topic')
     if end_ts != OFFSET_END:
         end_ts_dt = datetime.fromtimestamp(end_ts / 1000, tz=timezone.utc)
         logging.info(f'Stop reading at {end_ts_dt.strftime(DATE_FMT)}')
     else:
-        logging.info(f'Stop reading at end of topic')
+        logging.info('Stop reading at end of topic')
 
-    path_attributes = config.getkv_csv('filter',
-                                       'path_attributes',
-                                       fallback=None)
+    path_attributes = config.getkv_csv('filter', 'path_attributes', fallback=None)
 
     excluded_path_attributes = None
     if config.get('filter', 'excluded_path_attributes', fallback=None):
-        excluded_path_attributes = set(config.getcsv('filter',
-                                                     'excluded_path_attributes',
-                                                     fallback=None))
+        excluded_path_attributes = set(config.getcsv('filter', 'excluded_path_attributes', fallback=None))
 
     asn_filter = None
-    asn_list = config.get('filter', 'asn_list', fallback=None)
-    if asn_list:
-        asn_filter = read_asn_list(asn_list)
-        if asn_filter is None:
-            sys.exit(1)
+    if args.asn_filter:
+        asn_filter = handle_filter_arg(args.asn_filter, 'asn')
+    elif asn_list := config.get('filter', 'asn_list', fallback=None):
+        asn_filter = read_filter_list(asn_list, 'asn')
+    if asn_filter is not None and len(asn_filter) == 0:
+        logging.error('Empty ASN filter.')
+        sys.exit(1)
 
+    prb_filter = None
+    if args.probe_filter:
+        prb_filter = handle_filter_arg(args.probe_filter, 'probe')
+    elif prb_list := config.get('filter', 'prb_list', fallback=None):
+        prb_filter = read_filter_list(prb_list, 'probe')
+    if prb_filter is not None and len(prb_filter) == 0:
+        logging.error('Empty probe filter.')
+        sys.exit(1)
 
     bootstrap_servers = config.get('kafka', 'bootstrap_servers')
 
@@ -205,33 +218,33 @@ def main() -> None:
                              bootstrap_servers,
                              start_ts,
                              end_ts)
-    rib_writer = KafkaWriter(output_rib_topic, bootstrap_servers,
+    rib_writer = KafkaWriter(output_rib_topic,
+                             bootstrap_servers,
                              num_partitions=10,
                              # 2 months
                              config={'retention.ms': 5184000000})
     with rib_reader, rib_writer:
         last_ts = -1
         for msg in rib_reader.read():
-            filtered_msg, timestamp, prb_id = \
-              filter_msg(msg,
-                         path_attributes,
-                         excluded_path_attributes,
-                         asn_filter)
+            filtered_msg, timestamp, prb_id = filter_msg(msg,
+                                                         path_attributes,
+                                                         excluded_path_attributes,
+                                                         asn_filter,
+                                                         prb_filter)
             if filtered_msg:
                 if last_ts > timestamp:
-                    logging.warning(f'Writing out-of-order message: {last_ts} '
-                                    f'> {timestamp}')
+                    logging.warning(f'Writing out-of-order message: {last_ts} > {timestamp}')
                 key = None
                 if prb_id is not None:
-                    key = prb_id.to_bytes(4, byteorder='big')
+                    key = prb_id
+                    if isinstance(prb_id, int):
+                        key = prb_id.to_bytes(4, byteorder='big')
                 rib_writer.write(key, filtered_msg, timestamp * 1000)
                 last_ts = timestamp
 
     input_updates_topic = f'ihr_bgp_{config.get("input", "collector")}_updates'
-    output_updates_topic = \
-        f'ihr_bgp_{config.get("output", "collector")}_updates'
-    logging.info(f'Copying messages from updates topic {input_updates_topic} '
-                 f'to {output_updates_topic}')
+    output_updates_topic = f'ihr_bgp_{config.get("output", "collector")}_updates'
+    logging.info(f'Copying messages from updates topic {input_updates_topic} to {output_updates_topic}')
     if end_ts != OFFSET_END:
         # Updates topic message have an incremented timestamp.
         end_ts += 1000
@@ -239,7 +252,8 @@ def main() -> None:
                                  bootstrap_servers,
                                  start_ts,
                                  end_ts)
-    updates_writer = KafkaWriter(output_updates_topic, bootstrap_servers,
+    updates_writer = KafkaWriter(output_updates_topic,
+                                 bootstrap_servers,
                                  num_partitions=10,
                                  # 2 months
                                  config={'retention.ms': 5184000000})
@@ -256,8 +270,7 @@ def main() -> None:
     with updates_writer:
         for timestamp, msg in update_messages:
             if last_ts > timestamp:
-                logging.warning(f'Writing out-of-order message: {last_ts} '
-                                f'> {timestamp}')
+                logging.warning(f'Writing out-of-order message: {last_ts} > {timestamp}')
             updates_writer.write(None, msg, timestamp * 1000)
             last_ts = timestamp
 
